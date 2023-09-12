09ea872609a414e8290c6aea451da11372335df5
Martez Killens <martezkillens@gmail.com>
2023-09-11 17:40:59 -0700

    Upgarding project to .NET 6.0

--- WasmApp/Program.cs
+++ WasmApp/Program.cs
@@ -1,18 +1,19 @@
 using System;
-using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
-using System.Text;
 using BlazorAnimation;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
 
-namespace WasmApp {
-	public class Program {
+namespace WasmApp
+{
+    public class Program {
 		public static async Task Main(string[] args) {
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("app");
 
-			builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 			builder.Services.Configure<AnimationOptions>(Guid.NewGuid().ToString(),c=>{});
 
 			await builder.Build().RunAsync();
